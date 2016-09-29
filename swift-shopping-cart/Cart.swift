ec2fbd89e3c1c02925363ab3b4d64f32bbbf8c72
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-09-29 09:31:35 -0400

    Done

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,6 +7,56 @@
 //
 
 class Cart {
+    var items = [Item]()
     
+    func totalPriceInCents() -> Int {
+        var totalPrice = 0
+        for item in items {
+            totalPrice += item.priceInCents
+        }
+        return totalPrice
+    }
     
+    func add(item: Item){
+        items.append(item)
+    }
+    
+    func remove(item: Item){
+        for (index, value) in items.enumerated() {
+            if value == item {
+                items.remove(at: index)
+            }
+            
+        }
+    }
+    
+    func items(name: String) -> [Item]{
+        for item in items {
+            if item.name == name {
+                items.append(item)
+            }
+        }
+        return items
+    }
+    
+    func items(withMinPrice price:Int)-> [Item]{
+        for item in items {
+            if item.priceInCents >= price {
+                items.append(item)
+            }
+        }
+        return items
+    }
+
+    func items(withMaxPrice price:Int)-> [Item]{
+        for item in items {
+            if item.priceInCents <= price {
+                items.append(item)
+            }
+        }
+        return items
+    }
+
 }
+    
+
